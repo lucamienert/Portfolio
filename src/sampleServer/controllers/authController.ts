5fea6fac72fcfd9fee2d57d91d7b80523a00d32b
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-07-15 13:46:58 +0200

    Added Auth

--- src/sampleServer/controllers/authController.ts
+++ src/sampleServer/controllers/authController.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from 'express'
+import { CookieOptions, Request, Response } from 'express'
 import * as users from '../model/users.json'
 import { compare } from 'bcrypt'
 import { sign } from 'crypto'
+import { promises } from 'fs'
+import { join } from 'path'
 
 const userDB = {
     user: users,
@@ -25,8 +27,41 @@ const handleLogin = async (req: Request, res: Response) => {
 
         const accessToken = sign(
             {
-
-            }
+                UserInfo: {
+                    'username': foundUser.username,
+                    roles: roles
+                }
+            } as any,
+            process.env.ACCESS_TOKEN_SECRET as any,
+            { expiresIn: '30s'} as any
         )
-    }
+
+        const refreshToken = sign(
+            { "username": foundUser.username } as any,
+            process.env.REFRESH_TOKEN_SECRET as any,
+            { expiresIn: '1d' } as any
+        )
+
+        const otherUsers = userDB.user.filter((person: any) => person.username !== foundUser.username)
+        const currentUser = { ...otherUsers, refreshToken }
+
+        userDB.setUser([...otherUsers, currentUser])
+
+        await promises.writeFile(
+            join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(userDB.user)
+        )
+
+        const options: CookieOptions = {
+            httpOnly: true,
+			sameSite: 'none',
+			secure: true,
+			maxAge: 24 * 60 * 60 * 1000
+        }
+
+        res.cookie('jwt', refreshToken, options)
+        res.json({ accessToken })
+    } else res.sendStatus(401)
 }
+
+export default handleLogin
---
+++ src/sampleServer/routes/auth.ts
@@ -0,0 +1,8 @@
+import { Router } from "express"
+import handleLogin from "../controllers/authController"
+
+const router = Router()
+
+router.post('/', handleLogin)
+
+export default router
