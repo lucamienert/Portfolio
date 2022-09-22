3414dd5072df355a4b0b9efab6781b25029c5a08
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-09-22 12:12:50 +0200

    Delete src/sampleServer directory

--- src/sampleServer/config/allowedOrigins.ts
+++
@@ -1,7 +0,0 @@
-const allowedOrigins: any = [
-    'http://127.0.0.1:5500',
-	'http://localhost:3500',
-	'http://localhost:3000',
-]
-
-export default allowedOrigins
--- src/sampleServer/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import handleLogin from "../controllers/authController"
-
-const router = Router()
-
-router.post('/', handleLogin)
-
-export default router
